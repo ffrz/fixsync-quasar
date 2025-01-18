02276135b28d32173b3998a359f781fcdddcc12f
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-01-18 17:30:57 +0700

    split build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,21 @@ import vue from "@vitejs/plugin-vue";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor1: ['vue', 'quasar', 'dayjs', 'material-design-icons-iconfont', 'vue-i18n'],
+          vendor2: ['vue-echarts'],
+          vendor3: ['echarts'],
+          // components: [
+          //   '/resources/js/pages/admin/auth/Login.vue',
+          //   '/resources/js/pages/admin/auth/Register.vue',
+          // ],
+        },
+      },
+    },
+  },
     plugins: [
         vue({
             template: { transformAssetUrls },
